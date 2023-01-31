bdc03c9ba1b6609f4ecb37d5e62ea4eb774cf1d0
Ben Kirk <benkirk@ucar.edu>
2023-01-31 16:09:54 -0700

    mpi example on cheyenne

---
+++ examples/hello_world_mpi_ssend_recv.C
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <iomanip>
+#include <cassert>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <unistd.h>
+#include "mpi.h"
+
+
+
+int main (int argc, char **argv)
+{
+  int numranks, rank;
+  char hn[256];
+
+  gethostname(hn, sizeof(hn) / sizeof(char));
+
+  MPI_Init(&argc, &argv);
+
+  MPI_Comm_size (MPI_COMM_WORLD, &numranks);
+  MPI_Comm_rank (MPI_COMM_WORLD, &rank);
+
+  assert (numranks%2 == 0);
+
+  std::cout << "Hello from " << rank
+            << " / " << std::string (hn)
+	    << ", running " << argv[0] << " on "
+	    << numranks << " ranks"
+	    << std::endl;
+
+  MPI_Barrier(MPI_COMM_WORLD);
+
+  std::size_t cnt=1e6;
+  std::vector<int> buf(cnt);
+
+  if (0 == rank%2) // even sends
+    {
+      int i=rank;
+      std::for_each(buf.begin(), buf.end(), [&i](int &b) { b=i++; });
+
+      std::cout << "Rank " << rank << " sending [";
+      auto end = buf.begin();
+      std::advance(end,10);
+      std::for_each(buf.begin(), end, [](const int &b) { std::cout << b << " "; });
+      std::cout << "... ]" << std::endl;
+
+      // First, matched send/recv
+      {
+        std::cout << "calling MPI_Send";
+        MPI_Send (&buf[0], buf.size(), MPI_INT, /* dest = */ rank+1, /* tag = */ 100, MPI_COMM_WORLD);
+        std::cout << "...done\n";
+      }
+
+      // Next, matched isend/recv
+      {
+        MPI_Request req;
+        std::cout << "calling MPI_Isend";
+        MPI_Isend (&buf[0], buf.size(), MPI_INT, /* dest = */ rank+1, /* tag = */ 200, MPI_COMM_WORLD, &req);
+        MPI_Wait(&req, MPI_STATUS_IGNORE);
+        std::cout << "...done\n";
+      }
+
+      // Next, matched ssend/recv
+      {
+        std::cout << "calling MPI_Ssend";
+        MPI_Ssend (&buf[0], buf.size(), MPI_INT, /* dest = */ rank+1, /* tag = */ 300, MPI_COMM_WORLD);
+        std::cout << "...done\n";
+      }
+
+      // Next, matched isend/recv
+      {
+        MPI_Request req;
+        std::cout << "calling MPI_Issend";
+        MPI_Issend (&buf[0], buf.size(), MPI_INT, /* dest = */ rank+1, /* tag = */ 400, MPI_COMM_WORLD, &req);
+        MPI_Wait(&req, MPI_STATUS_IGNORE);
+        std::cout << "...done\n";
+      }
+    }
+
+  else // odd recvs
+    {
+      MPI_Recv (&buf[0], buf.size(), MPI_INT, /* dest = */ rank-1, /* tag = */ 100, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+      MPI_Recv (&buf[0], buf.size(), MPI_INT, /* dest = */ rank-1, /* tag = */ 200, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+      MPI_Recv (&buf[0], buf.size(), MPI_INT, /* dest = */ rank-1, /* tag = */ 300, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+      MPI_Recv (&buf[0], buf.size(), MPI_INT, /* dest = */ rank-1, /* tag = */ 400, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+      std::cout << "Rank " << rank << " recvs [";
+      auto end = buf.begin();
+      std::advance(end,10);
+      std::for_each(buf.begin(), end, [](const int &b) { std::cout << b << " "; });
+      std::cout << "... ]" << std::endl;
+
+    }
+
+  MPI_Finalize();
+
+  return 0;
+}
---
+++ examples/hello_world_mpi_ssend_recv.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#PBS -q regular
+#PBS -A SCSG0001
+#PBS -l walltime=00:30:00
+#PBS -l select=1:ncpus=12:mpiprocs=12
+#PBS -j oe
+
+module reset >/dev/null 2>&1
+#module unload cuda    >/dev/null 2>&1
+#module unload openmpi >/dev/null 2>&1
+#module load nvhpc/22.5 >/dev/null 2>&1 || { echo "cannot find requested module!"; exit 1; }
+module list
+
+mpicxx -o hello_world_mpi_ssend_recv hello_world_mpi_ssend_recv.C -std=c++11 || exit 1
+
+export LOG_MEMUSAGE_VERBOSE=1
+export LOG_MEMUSAGE_ENABLE_LOGFILE=1
+mpiexec_mpt \
+    LD_PRELOAD=/glade/work/benkirk/peak_memusage/chey/install/lib/liblog_memusage.so \
+    ./ hello_world_mpi_ssend_recv
+
+echo && echo && echo "Done at $(date)"
