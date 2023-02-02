12bc252d033d3e1ae8b1c91cf8813f22a453e469
Ben Kirk <benkirk@ucar.edu>
2023-02-01 17:19:49 -0700

    WIP: malloc wrapper

--- log_memusage_malloc.c
+++ log_memusage_malloc.c
@@ -1,7 +1,7 @@
-
 #ifdef HAVE_CONFIG_H
 #  include "config.h"
 #endif
+#define _GNU_SOURCE
 #include <stdio.h>	/* print */
 #include <stdlib.h>	/* malloc */
 #include <unistd.h>     /* optarg */
@@ -9,7 +9,23 @@
 #include <dlfcn.h>
 
 
-static void* (*real_malloc)(size_t) = NULL;
+static void* (*real_malloc) (size_t) = NULL;
+static void  (*real_free)   (void*)  = NULL;
+
+
+
+static void mtrace_init (void)
+{
+  fprintf(stderr, "..(init)... %s, line: %d\n", __FILE__, __LINE__);
+
+  real_malloc = dlsym(RTLD_NEXT, "malloc");
+  real_free   = dlsym(RTLD_NEXT, "free");
+
+  if (NULL == real_malloc)
+    {
+      fprintf(stderr, "Error in `dlsym`: %s\n", dlerror());
+    }
+}
 
 
 
@@ -18,32 +34,34 @@ __attribute__ ((visibility ("hidden")))
 void init ()
 {
   fprintf(stderr, "..(constructor)... %s, line: %d\n", __FILE__, __LINE__);
+
+  mtrace_init();
   return;
-}
-
-
-
-static void mtrace_init (void)
-{
-  fprintf(stderr, "..(init)... %s, line: %d\n", __FILE__, __LINE__);
-  real_malloc = dlsym(RTLD_NEXT, "malloc");
-
-  if (NULL == real_malloc) {
-    fprintf(stderr, "Error in `dlsym`: %s\n", dlerror());
-  }
 }
 
 
 
 void *malloc (size_t size)
 {
-  if(real_malloc==NULL) {
-    mtrace_init();
-  }
+  if (NULL == real_malloc)
+    {
+      mtrace_init();
+    }
 
   void *p = NULL;
   fprintf(stderr, "malloc(%ld) = ", size);
   p = real_malloc(size);
   fprintf(stderr, "%p\n", p);
   return p;
+}
+
+
+
+void free (void *buf)
+{
+  if (NULL == real_free)
+    mtrace_init();
+
+  fprintf(stderr, "free(%p)\n", buf);
+  real_free(buf);
 }
